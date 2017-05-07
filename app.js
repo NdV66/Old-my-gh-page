a239429b01a55d2b81fd86bfd42d7434318de72e
Marta Zażlak <marta.zazlak@hotmail.com>
2017-05-07 14:10:21 +0200

    release 3.0

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 require.config({
     shim : {
-        bootstrap: { 
+        bootstrap: {
             deps: ['jquery']
         }
     },
     paths: {
         View: "lib/modules/view.min",
-        Model: "lib/modules/model.min",
         Controller: "lib/modules/controller.min",
         jquery: "lib/jquery",
         bootstrap: "lib/bootstrap.min"
     }
 });
 
-requirejs(['js/script']);
+requirejs(['js/script.min']);
